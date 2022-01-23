efb5a360b50221e20833ec72e98aa339c82e1063
Hamza <mhamza@ryerson.ca>
2022-01-22 20:51:50 -0500

    added display functionality. Props used to pass variables and functions to different functions

--- 1b_incrementButton.jsx
+++ 1b_incrementButton.jsx
@@ -1,9 +1,30 @@
-//button to increment by 1
-function Button() {
-    const [counter, setCounter] = useState(0)
-    return <button onClick={() => setCounter(counter + 1)} >{counter}</button>;
+function Button(props) {
+    // const handleClick = () => setCounter(counter+1)
+    return (
+        <button onClick={props.onClickFunction}>
+            +1
+        </button>
+    );
 }
+
+function Display(props) {
+    return (
+        <div>{props.message}</div>
+    );
+}
+
+function App() {
+    const [counter, setCounter] = useState(0);
+    const incrementCounter = () => setCounter(counter + 1);
+    return (
+        <div>
+            <Button onClickFunction={incrementCounter} />
+            <Display message={counter} />
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <Button />,
-    document.getElementById('mountNode')
+    <App />,
+    document.getElementById('mountNode'),
 );
